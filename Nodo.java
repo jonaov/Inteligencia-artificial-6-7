228166190a2519081554eb98aa802ed6206c4a15
jonaov <114442092+jonaov@users.noreply.github.com>
2023-05-08 17:36:31 -0600

    Add files via upload

---
+++ Nodo.java
@@ -0,0 +1,12 @@
+package binario;
+//creamos la clase nodo
+public class Nodo {
+    int valor;
+    Nodo izquierdo, derecho;
+
+    public Nodo(int valor) {
+        this.valor = valor;
+        izquierdo = null;
+        derecho = null;
+    }
+}
